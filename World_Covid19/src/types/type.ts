271be4ec7a3a2801216de00073d6cd5d11bae7ae
luminousxv <drgvyhn@gmail.com>
2022-07-21 18:07:53 +0900

    Postman Covid-19 API 리턴 타입 정의

---
+++ World_Covid19/src/types/type.ts
@@ -0,0 +1,44 @@
+export interface Summary {
+  Global: Global;
+  Countries: Countries[];
+}
+
+interface Global {
+  NewConfirmed: number;
+  TotalConfirmed: number;
+  NewDeaths: number;
+  TotalDeaths: number;
+  NewRecovered: number;
+  TotalRecovered: number;
+}
+
+export interface Countries extends Global {
+  Country: string;
+  CountryCode: string;
+  Slug: string;
+  Date: number;
+}
+
+export interface LiveCountryInfo {
+  Country: string;
+  CountryCode: string;
+  Cases: number;
+  Status: string;
+  Date: string;
+}
+
+export interface Confirmed {
+  Country: string;
+  CountryCode: string;
+  NewConfirmed: number;
+  TotalConfirmed: number;
+}
+
+export interface Deaths {
+  Country: string;
+  CountryCode: string;
+  NewDeaths: number;
+  TotalDeaths: number;
+}
+
+export type StatusParams = 'confirmed' | 'deaths';
