29fa136665491d2c0e054a6fa9abe50c20f08187
luminousxv <drgvyhn@gmail.com>
2022-07-24 17:57:34 +0900

    Added US States Flatlist (required in COVID-19 api)

---
+++ World_Covid19/src/component/UI/atom/state_flatlist.tsx
@@ -0,0 +1,49 @@
+import React, {ReactElement} from 'react';
+import {View, StyleSheet, FlatList, Text} from 'react-native';
+import {Modal_Screen_Props} from '../../../types/navigation/navigation_type';
+import {states} from '../../../api/Covid19_API';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+export default function ModalScreen({
+  route,
+  navigation,
+}: Modal_Screen_Props): ReactElement {
+  const {status} = route.params;
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={states}
+        renderItem={({item}) => (
+          <TouchableOpacity
+            style={styles.item}
+            onPress={() => {
+              navigation.navigate('CountryInfo', {
+                country: item,
+                status: status,
+                country_title: item,
+              });
+            }}>
+            <Text>{item}</Text>
+          </TouchableOpacity>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ADD8E6',
+  },
+  item: {
+    padding: 10,
+    fontWSize: 20,
+    height: 60,
+    borderWidth: 10,
+    borderColor: '#ADD8E6',
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
