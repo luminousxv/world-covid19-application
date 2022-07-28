08847b635c677d02a1b33e3ab482a8be24dbd0f4
luminousxv <drgvyhn@gmail.com>
2022-07-28 17:48:18 +0900

    added Detailed Screen when touched TouchOpacity Component

--- World_Covid19/src/component/UI/atom/newcases.tsx
+++ World_Covid19/src/component/UI/atom/newcases.tsx
@@ -1,26 +1,43 @@
+import {useNavigation} from '@react-navigation/native';
 import React, {ReactElement, useState} from 'react';
 import {StyleSheet, View, Text, FlatList} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
-import {TableProp} from '../../../types/navigation/navigation_type';
+import {
+  DetailedScreenProp,
+  TableProp,
+} from '../../../types/navigation/navigation_type';
 
 export default function NewCasesView({
   country,
   newcases,
   total,
   date,
+  cases,
+  datearray,
+  status,
 }: TableProp): ReactElement {
   const [data] = useState<string[]>([
     `Country / States: ${country}`,
-    `New: ${newcases}`,
+    `New (By Country): ${newcases}`,
     `Total: ${total}`,
     `Update Date: ${date}`,
   ]);
+  const navigation = useNavigation<DetailedScreenProp>();
   return (
     <View>
       <FlatList
         data={data}
         renderItem={({item}) => (
-          <TouchableOpacity style={styles.item}>
+          <TouchableOpacity
+            style={styles.item}
+            onPress={() => {
+              navigation.navigate('DetailedScreen', {
+                country: country,
+                cases: cases,
+                date: datearray,
+                status: status,
+              });
+            }}>
             <Text>{item}</Text>
           </TouchableOpacity>
         )}
@@ -32,9 +49,9 @@ export default function NewCasesView({
 const styles = StyleSheet.create({
   item: {
     padding: 10,
-    fontWSize: 20,
-    height: 60,
-    borderWidth: 10,
+    fontSize: 15,
+    height: 50,
+    borderWidth: 5,
     borderColor: '#ffffff',
     backgroundColor: '#ffffff',
     alignItems: 'center',
