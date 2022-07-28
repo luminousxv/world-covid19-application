87319b3953f4cc57f78394adee930fe2a3247013
luminousxv <drgvyhn@gmail.com>
2022-07-28 17:47:05 +0900

    Added Stack.Screen for Detailed Screen

--- World_Covid19/App.tsx
+++ World_Covid19/App.tsx
@@ -6,6 +6,7 @@ import TabScreen from './src/component/pages/TabScreen';
 import CountryScreen from './src/component/UI/molecule/CountryScreen';
 import {RootStackParamList} from './src/types/navigation/navigation_type';
 import FlatListScreen from './src/component/UI/atom/state_flatlist';
+import DetailedScreen from './src/component/UI/molecule/DetailedScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -31,6 +32,18 @@ function App() {
             component={FlatListScreen}
             initialParams={{status: 'confirmed'}}
             options={{title: 'Choose State'}}
+          />
+          <Stack.Screen
+            name="DetailedScreen"
+            component={DetailedScreen}
+            options={({route}) => ({
+              title:
+                route.params.country +
+                ' ' +
+                route.params.status +
+                ' ' +
+                'Details',
+            })}
           />
         </Stack.Navigator>
       </NavigationContainer>
